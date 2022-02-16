84c1bc72ed02df035bbc8c12d882e9ae92f39152
AltugAltintas <altuga@gmail.com>
2022-02-16 07:22:53 +0300

    second commit

---
+++ src/main/java/jug/istanbul/demos/security/AuditEventListener.java
@@ -0,0 +1,32 @@
+package jug.istanbul.demos.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.audit.*;
+import org.springframework.boot.actuate.audit.listener.AbstractAuditListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Replaces the default org.springframework.boot.actuate.audit.listener.AuditListener.
+ */
+@Component
+public class AuditEventListener extends AbstractAuditListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuditEventListener.class);
+
+    private final AuditEventRepository auditEventRepository = new InMemoryAuditEventRepository();
+
+    @Override
+    protected void onAuditEvent(AuditEvent event) {
+
+        LOG.info("On audit event: timestamp: {}, principal: {}, type: {}, data: {}",
+                event.getTimestamp(),
+                event.getPrincipal(),
+                event.getType(),
+                event.getData()
+        );
+
+        auditEventRepository.add(event);
+    }
+
+}
